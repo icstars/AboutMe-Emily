1b988cdd480b4dc320f6ddc9938149c046da972b
ehornbeck <149593424+ehornbeck@users.noreply.github.com>
2024-01-08 13:05:20 -0600

    Form functionality
    adding onto the form functionality

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 //firstname
 const grabFirstNameInput = document.getElementById("firstNameInput");
-console.log(grabFirstNameInput);
+//console.log(grabFirstNameInput);
 //lastName
 const grabLastNameInput = document.getElementById("lastNameInput");
-console.log(grabLastNameInput);
+//console.log(grabLastNameInput);
 //birthDate
 const grabemailInput= document.getElementById("emailInput");
-console.log(grabemailInput);
+//console.log(grabemailInput);
 //message
 const grabmessageInput = document.getElementById("messageInput");
-console.log(grabmessageInput);
+//console.log(grabmessageInput);
 //SubmitLIKE
 const grabSubmitBtn = document.querySelector(".buttonForm");
-console.log(grabSubmitBtn);
+//console.log(grabSubmitBtn);
 
 grabSubmitBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    console.log("this is the Send Message button")
-    console.log(grabFirstNameInput.value);
-    console.log(grabLastNameInput.value);
-    console.log(grabemailInput.value);
-    console.log(grabmessageInput.value);
+    //console.log("this is the Send Message button")
+    //console.log(grabFirstNameInput.value);
+    //console.log(grabLastNameInput.value);
+    //console.log(grabemailInput.value);
+    //console.log(grabmessageInput.value);
     //paragraph element
     const paragraphElement = document.querySelector(".paragraphResponse")
     const imageElement = document.getElementById("imageResponse")
@@ -29,7 +29,7 @@ grabSubmitBtn.addEventListener("click", (e) => {
     paragraphElement.textContent = `Thank you for your message ${grabFirstNameInput.value} ${grabLastNameInput.value}. I'll be in touch soon!`
    
    
-    console.log(`Thank you for your message ${grabFirstNameInput.value} ${grabLastNameInput.value}. I'll be in touch soon!`)
+    //console.log(`Thank you for your message ${grabFirstNameInput.value} ${grabLastNameInput.value}. I'll be in touch soon!`)
 
     //clear values
     grabFirstNameInput.value = "";
@@ -42,6 +42,39 @@ grabSubmitBtn.addEventListener("click", (e) => {
     imageElement.style.display = "block"; //makes the image visible
 })
 
+//constructor function
+function Person(firstName, lastName, eMail, message){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.eMail = eMail;
+    this.message = message;
+}
 
+
+const grabForm = document.querySelector("form");
+console.log(grabForm);
+
+const peopleArrayOfObjects = [];
+
+grabForm.addEventListener("submit",(e) => {
+    e.preventDefault();
+    const formData = new FormData(grabForm);
+    console.log(formData)
+
+    const firstName = formData.get("firstName");
+    console.log(firstName);
+
+    const lastName = formData.get("lastName");
+    console.log(lastName);
+
+    const eMail = formData.get("eMail");
+    console.log(eMail);
+
+    const message = formData.get("message");
+    console.log(message);
+
+    const newPerson = new Person(firstName, lastName, eMail, message,);
+    console.log(newPerson)
+});
 
 
